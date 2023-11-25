b66364982cc078737bcc3dad7446e54fb35bc022
Rohit K <rohit.kumar.877033@gmail.com>
2023-11-25 17:28:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+const log = require('./services/logger.service.js')
+
+const port = process.env.PORT || 8000
+const app = express()
+
+app.listen(port, () => {
+  log.info(`Server is running on http://localhost:${port}`)
+})
